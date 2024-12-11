4ff00816f691d14435caf58cdbeb53edc264644b
ryaaahs <reily.maahs@gmail.com>
2024-12-10 22:55:15 -0500

    Session 7: Printf format string

---
+++ printing.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+    float x, y;
+
+    i = 2;
+    j = 20;
+    x = 43.2892f;
+    y = 5527.0f;
+
+
+    // NOTE: C compiler is not required to check the total amount of conversion specs or
+    // expressions past to the function, which can cause issues!
+    // Or if the conversion spec is appropriate for the expression being printed, it will produce
+    // garbage output.
+
+    // Bad examples
+    // printf("%d %d\n", i);
+    // printf("%d\n", i, j); 
+    // printf("%f %d\n", i, x);
+
+    // Conversions can have this format %m.pX or %-m.pX (m/p are optional int constants)
+    // X is the format letter
+
+    // m == minimum field width. Specifies the min number of characters to print (Fills with spaces)
+    // "•" space character
+    // p == precision depends on the conversion specifier
+
+    // Common Converstion Specifiers
+    // d - displays an int in decimal form (base10) (p indicates the minimum amount of numbers to display)
+    // (Adding extra zeros to the start of the number)
+    // e
+    // f
+    // g
+
+    printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+    printf("i = %.2d\n", i); //••••••••10 %10d - %-4d == 10••
+    printf("i = %-4da\n", i); // If the value is 10000, it will display the value normally
+}
